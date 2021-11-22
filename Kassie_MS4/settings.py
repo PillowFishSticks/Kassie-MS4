85eb6d67e2ee66a8ec6655ccba14d0ecb1a17f90
PillowFishSticks <markcpercy@gmail.com>
2021-11-22 13:55:31 +0000

    added checkout views and templates

--- Kassie_MS4/settings.py
+++ Kassie_MS4/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Kassie_MS4.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +78,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required buy allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
