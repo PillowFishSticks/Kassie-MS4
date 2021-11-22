e2fa39e4ac7bb6f66d9155b023fb3a3901eba378
PillowFishSticks <markcpercy@gmail.com>
2021-11-22 16:33:35 +0000

    style checkout success page

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -63,6 +63,8 @@ form.addEventListener('submit', function(ev) {
                 </span>
                 <span>${result.error.message}</span>`;
             $(errorDiv).html(html);
+    
+    
             card.update({ 'disabled': false});
             $('#submit-button').attr('disabled', false);
         } else {
