5c1fa05856e52a319141538caac47ca1b992e2b1
PillowFishSticks <markcpercy@gmail.com>
2021-11-22 15:39:02 +0000

    add env file for secret key

--- Kassie_MS4/settings.py
+++ Kassie_MS4/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-nuzut2394d+jop9g1r1#$!jpa(%38)nl9lw5_2(qhdnp)b2abu'
--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+if os.path.exists("env.py"):
+    import env
 
 
 def main():
