443d23d9498b852735f8b6069036c0e5d7b621a6
PillowFishSticks <markcpercy@gmail.com>
2021-11-27 07:30:46 +0000

    add url for reviews

--- Kassie_MS4/urls.py
+++ Kassie_MS4/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
     path('inspo/', include('inspo.urls')),
+    path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
