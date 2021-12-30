e1a2d6a2da61524c966f2c33f6957432154d9123
PillowFishSticks <markcpercy@gmail.com>
2021-12-30 05:02:49 +0000

    Add page banner to allauth pages

--- Kassie_MS4/settings.py
+++ Kassie_MS4/settings.py
@@ -206,11 +206,11 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'Kassie@example.com'
+DEFAULT_FROM_EMAIL = 'Kassie'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'kassie@example.com'
+    DEFAULT_FROM_EMAIL = 'Kassie'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
